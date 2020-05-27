706b34eeb49d0300a18a1d8f0df66ff5cd7c20d4
Robbert van der Helm <mail@robbertvanderhelm.nl>
2020-05-27 15:44:14 +0200

    Rearrange order of Win32 and X11 event handling
    Should not matter that much, but a potential situation where you would
    want to have handled the X11 events first is when the editor enters a
    blocking message loop while it is waiting on a GUI component just as the
    window gets moved by an external program or window manager.

--- src/wine-host/bridges/vst2.cpp
+++ src/wine-host/bridges/vst2.cpp
@@ -162,8 +162,8 @@ void Vst2Bridge::handle_dispatch_single() {
                               plugin, std::bind(&Vst2Bridge::dispatch_wrapper,
                                                 this, _1, _2, _3, _4, _5, _6)));
 
-            handle_win32_events();
             handle_x11_events();
+            handle_win32_events();
         }
     } catch (const boost::system::system_error&) {
         // The plugin has cut off communications, so we can shut down this host
