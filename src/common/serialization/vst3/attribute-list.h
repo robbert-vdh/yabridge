3566aa86a2a8302d53214c901f0c7c03895e1c3a
Robbert van der Helm <mail@robbertvanderhelm.nl>
2020-12-25 12:58:18 +0100

    Fix IAttributeList serialization

--- src/common/serialization/vst3/attribute-list.h
+++ src/common/serialization/vst3/attribute-list.h
@@ -61,22 +61,22 @@ class YaAttributeList : public Steinberg::Vst::IAttributeList {
     template <typename S>
     void serialize(S& s) {
         s.ext(attrs_int, bitsery::ext::StdMap{1 << 20},
-              [](S& s, AttrID& key, int64& value) {
+              [](S& s, std::string& key, int64& value) {
                   s.text1b(key, 1024);
                   s.value8b(value);
               });
         s.ext(attrs_float, bitsery::ext::StdMap{1 << 20},
-              [](S& s, AttrID& key, double& value) {
+              [](S& s, std::string& key, double& value) {
                   s.text1b(key, 1024);
                   s.value8b(value);
               });
         s.ext(attrs_string, bitsery::ext::StdMap{1 << 20},
-              [](S& s, AttrID& key, double& value) {
+              [](S& s, std::string& key, std::u16string& value) {
                   s.text1b(key, 1024);
                   s.text2b(value, 1 << 20);
               });
         s.ext(attrs_binary, bitsery::ext::StdMap{1 << 20},
-              [](S& s, AttrID& key, std::vector<uint8_t>& value) {
+              [](S& s, std::string& key, std::vector<uint8_t>& value) {
                   s.text1b(key, 1024);
                   s.container1b(value, 1 << 20);
               });
