3d690a370bccb4c32f14dd81c71eeb9adde9e0b5
Robbert van der Helm <mail@robbertvanderhelm.nl>
2021-01-07 17:34:44 +0100

    Change the todo about IComponentHandler3
    This is actually supported by hosts. It's just that plugins don't seem
    to use it.

--- src/common/serialization/vst3/context-menu-proxy.h
+++ src/common/serialization/vst3/context-menu-proxy.h
@@ -35,7 +35,8 @@
  * implements `IComponentHandler3` and thus provides support for these context
  * menus.
  *
- * TODO: None of this has been tested because no host on Linux implement this.
+ * TODO: None of this has been tested because I have not run into any plugins
+ *       that use `IComponentHandler3` yet.
  */
 class Vst3ContextMenuProxy : public YaContextMenu {
    public:
