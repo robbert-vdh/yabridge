e1975c0795b798f75e2a5e93b10cbf445973cb66
Robbert van der Helm <mail@robbertvanderhelm.nl>
2021-04-03 18:15:25 +0200

    Update VST3 SDK #include <*filesystem> patches

--- tools/patch-vst3-sdk.sh
+++ tools/patch-vst3-sdk.sh
@@ -76,9 +76,8 @@ sed -i 's/\bgeneric_u8string\b/generic_string/g' "$sdk_directory/public.sdk/sour
 # libstdc++fs doesn't work under Winelib, for whatever reason that might be.
 # We'll patch the Win32 module loading to use Boost.Filesystem instead.
 sed -i 's/^#include <\(experimental\/\)\?filesystem>$/#include <boost\/filesystem.hpp>/' "$sdk_directory/public.sdk/source/vst/hosting/module_win32.cpp"
-sed -i 's/^using namespace std\(::experimental\)\?;$/namespace filesystem = boost::filesystem;/' "$sdk_directory/public.sdk/source/vst/hosting/module_win32.cpp"
+sed -i 's/^namespace filesystem = std\(::experimental\)\?::filesystem;$/namespace filesystem = boost::filesystem;/' "$sdk_directory/public.sdk/source/vst/hosting/module_win32.cpp"
 sed -i 's/\bfile_type::directory\b/file_type::directory_file/g' "$sdk_directory/public.sdk/source/vst/hosting/module_win32.cpp"
-sed -i 's/\bp\.native ()/p.wstring ()/g' "$sdk_directory/public.sdk/source/vst/hosting/module_win32.cpp"
 
 # Wine uses the narrow versions of everything by default, and in Unity builds
 # we need to explicitly use the UTF-16 version here.
