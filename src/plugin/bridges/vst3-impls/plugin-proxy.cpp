e0ab24e64581dcd3c6367054ab670a8b7201b5d9
Robbert van der Helm <mail@robbertvanderhelm.nl>
2022-01-01 17:20:33 +0100

    Update copyright headers
    Happy new year!

--- src/plugin/bridges/vst3-impls/plugin-proxy.cpp
+++ src/plugin/bridges/vst3-impls/plugin-proxy.cpp
@@ -1,5 +1,5 @@
 // yabridge: a Wine VST bridge
-// Copyright (C) 2020-2021 Robbert van der Helm
+// Copyright (C) 2020-2022 Robbert van der Helm
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -15,6 +15,8 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include "plugin-proxy.h"
+
+#include <pluginterfaces/vst/ivstmidicontrollers.h>
 
 #include "plug-view-proxy.h"
 
