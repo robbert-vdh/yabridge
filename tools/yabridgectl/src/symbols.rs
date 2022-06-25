cacb6ecff8829c9cf1cda0149adcaa7dca617548
Robbert van der Helm <mail@robbertvanderhelm.nl>
2022-06-25 13:53:30 +0200

    [yabridgectl] Add another hint to parsing errors

--- tools/yabridgectl/src/symbols.rs
+++ tools/yabridgectl/src/symbols.rs
@@ -96,7 +96,9 @@ fn parse_pe32_winedump<P: AsRef<Path>>(binary: P) -> Result<Pe32Info> {
             }
             Err(_) => None,
         })
-        .ok_or_else(|| anyhow!("Winedump output did not contain a 'Machine:' line"))?;
+        .ok_or_else(|| {
+            anyhow!("Winedump output did not contain a 'Machine:' line. Is a text file?")
+        })?;
 
     // And we'll just parse _all_ exported functions. Previously we would only check whether this
     // contrained certain entries, but efficiency isn't too important here anyways since this is
