d1b3de5fc00ac47e8c301099051f10547355c747
Robbert van der Helm <mail@robbertvanderhelm.nl>
2022-10-28 16:01:43 +0200

    Connect to the session message bus using libdbus-1
    This is the first step of migrating the desktop notifications over to
    pure DBus.

--- src/common/notifications.cpp
+++ src/common/notifications.cpp
@@ -16,14 +16,115 @@
 
 #include "notifications.h"
 
-#include <sstream>
+#include <atomic>
+#include <cassert>
+#include <iostream>
 
+#include <dbus/dbus.h>
+#include <dlfcn.h>
+
+#include "logging/common.h"
 #include "process.h"
 #include "utils.h"
+
+constexpr char libdbus_library_name[] = "libdbus-1.so";
+
+std::atomic<void*> libdbus_handle = nullptr;
+std::mutex libdbus_mutex;
+
+// We'll fetch all of these functions at runtime when send a first notification
+decltype(dbus_connection_unref)* libdbus_connection_unref = nullptr;
+decltype(dbus_bus_get)* libdbus_bus_get = nullptr;
+decltype(dbus_error_free)* libdbus_error_free = nullptr;
+decltype(dbus_error_init)* libdbus_error_init = nullptr;
+decltype(dbus_error_is_set)* libdbus_error_is_set = nullptr;
+decltype(dbus_connection_set_exit_on_disconnect)*
+    libdbus_connection_set_exit_on_disconnect = nullptr;
+
+std::unique_ptr<DBusConnection, void (*)(DBusConnection*)> libdbus_connection(
+    nullptr,
+    libdbus_connection_unref);
+
+/**
+ * Try to set up DBus. Returns `false` if a function could not be resolved or if
+ * we could not connect to the DBus session.
+ */
+bool setup_libdbus() {
+    // If this function is called from two threads at the same time, then we can
+    // skip this. `libdbus_handle` is only set at the very end of this function
+    // once every function pointer has been resolved
+    std::lock_guard lock(libdbus_mutex);
+    if (libdbus_handle) {
+        return true;
+    }
+
+    Logger logger = Logger::create_exception_logger();
+
+    void* handle = dlopen(libdbus_library_name, RTLD_LAZY | RTLD_LOCAL);
+    if (!handle) {
+        logger.log("Could not load '" + std::string(libdbus_library_name) +
+                   "', not sending desktop notifications");
+        return false;
+    }
+
+#define LOAD_FUNCTION(name)                                                 \
+    do {                                                                    \
+        lib##name =                                                         \
+            reinterpret_cast<decltype(lib##name)>(dlsym(handle, #name));    \
+        if (!(name)) {                                                      \
+            logger.log("Could not find '" + std::string(#name) + "' in '" + \
+                       std::string(libdbus_library_name) +                  \
+                       "', not sending desktop notifications");             \
+            return false;                                                   \
+        }                                                                   \
+    } while (false)
+
+    LOAD_FUNCTION(dbus_connection_unref);
+    LOAD_FUNCTION(dbus_bus_get);
+    LOAD_FUNCTION(dbus_error_free);
+    LOAD_FUNCTION(dbus_error_init);
+    LOAD_FUNCTION(dbus_error_is_set);
+    LOAD_FUNCTION(dbus_connection_set_exit_on_disconnect);
+
+#undef LOAD_FUNCTION
+
+    // With every function ready, we can try connecting to the DBus interface
+    DBusError error;
+    libdbus_error_init(&error);
+
+    libdbus_connection.reset(
+        libdbus_bus_get(DBusBusType::DBUS_BUS_SESSION, &error));
+    if (libdbus_error_is_set(&error)) {
+        assert(error.message);
+        logger.log("Could not connect to DBus session bus: " +
+                   std::string(error.message));
+        libdbus_error_free(&error);
+
+        return false;
+    }
+    assert(libdbus_connection);
+
+    // While the connection should not be closed while this plugin is alive,
+    // this does sound extremely dangerous and why is it enabled by default?
+    libdbus_connection_set_exit_on_disconnect(libdbus_connection.get(), false);
+
+    // This is only set at the very end since this indicates that everything
+    // has been initialized properly
+    libdbus_handle.store(handle);
+
+    return true;
+}
 
 bool send_notification(const std::string& title,
                        const std::string body,
                        std::optional<ghc::filesystem::path> origin) {
+    // The first time this function is called we'll need to set up the DBus
+    // interface. Previously yabridge relied on notify-send, but some distros
+    // don't install that by default.
+    if (!libdbus_handle && !setup_libdbus()) {
+        return false;
+    }
+
     // I think there's a zero chance that we're going to call this function with
     // anything that even somewhat resembles HTML, but we should still do a
     // basic XML escape anyways.
